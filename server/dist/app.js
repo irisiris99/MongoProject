dcf14bd7e5ae936c5a0c6156e8354c3164e4fd5c
irisiris99 <cjg03117@gmail.com>
2022-03-10 01:35:08 +0900

    dd;

--- server/dist/app.js
+++ server/dist/app.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -15,39 +6,36 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const mysql = require('mysql2/promise');
-let connection;
-const database = [
-    { id: 1, title: '글1' },
-    { id: 2, title: '글2' },
-    { id: 3, title: '글3' },
-];
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 app.use(express_1.default.static('build'));
 app.use(express_1.default.json());
-app.use(express_1.default.urlencoded({ extended: false }));
+app.use(cors());
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/build/index.html');
 });
-app.get('/database', function (req, res) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const [rows, fields] = yield connection.excute(`SELECT * FROM user`);
-        console.log('rows : ', rows);
-        res.send(rows);
-    });
+app.listen(PORT, () => {
+    console.log('server running !');
 });
+<<<<<<< HEAD
 app.post('/database', function (req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         const { name, age } = req.body;
         const [rows, fields] = yield connection.excute(`INSERT INTO user(name, age) VALUES(?, ?)`, [name, age]);
         res.send('값이 추가되었습니다..');
     });
+=======
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "passwrod",
+    database: "LoginSystem",
+>>>>>>> parent of 25bedbb (dd)
 });
-app.listen(PORT, () => __awaiter(void 0, void 0, void 0, function* () {
-    connection = yield mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        database: 'myapp',
-        password: 'root',
+app.post("/register", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    db.query("INSERT INTO users (username, password) VALUES (?,?)", [username, password], (err, result) => {
+        console.log(err);
     });
-    console.log('server running !');
-}));
+});
