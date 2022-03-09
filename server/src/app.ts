c7b91d5cec97c92e672ac79eac60b3e33d5d8da9
irisiris99 <cjg03117@gmail.com>
2022-03-09 15:27:52 +0900

    0309 db mysql

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,14 +3,32 @@ import express from 'express';
 const app = express();
 const mysql = require('mysql2/promise');
 
+
+let connection :any;
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('build'));
+
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/build/index.html');
 });
 
-app.listen(PORT, () => {
+
+app.get('/database', async function (req, res) {
+  console.log('connection :', connection);
+  const [rows, fields] = await connection.excute(`SELECT * FROM user`);
+  console.log('rows :', rows);
+  res.send(rows);
+})
+
+app.listen(PORT, async () => {
+  connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'myapp',
+    password: 'root',
+  });
   console.log('server running !');
 });
