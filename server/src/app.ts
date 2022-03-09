eb70aaa47b9bd6e7e4370f3432c16202f2a56a7b
irisiris99 <cjg03117@gmail.com>
2022-03-09 16:17:59 +0900

    reset

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,34 +1,15 @@
 import express from 'express';
 
 const app = express();
-const mysql = require('mysql2/promise');
 
-
-let connection :any;
-
-const PORT = process.env.PORT || 3006;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('build'));
-
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/build/index.html');
 });
 
-
-app.get('/database', async function (req, res) {
-  console.log('connection :', connection);
-  const [rows, fields] = await connection.excute(`SELECT * FROM user`);
-  console.log('rows :', rows);
-  res.send(rows);
-})
-
-app.listen(PORT, async () => {
-  connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'myapp',
-    password: 'root',
-  });
+app.listen(PORT, () => {
   console.log('server running !');
 });
