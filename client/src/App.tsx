ff0cab446001533fad0f32dd4455f00b891dc495
irisiris99 <cjg03117@gmail.com>
2022-03-12 23:02:59 +0900

    a2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div>
       <h1>express + heroku 서비스 자동 배포 (완료)</h1>
-      <h1>MySQL 연동해서 간단한 CRUD Market 구현하기 (작업 중)</h1>
+      <h1>MySQL 연동해서 간단한 CRUD 게시판 구현하기 (작업 중)</h1>
     </div>
   );
 }
