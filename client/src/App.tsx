822b929be42f3dc93658dffe1c9527eb4494b700
irisiris99 <cjg03117@gmail.com>
2022-03-15 16:57:58 +0900

    name

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,9 +5,10 @@ import './App.css';
 function App() {
 
   return (
-    <div>
-      <h1>express + heroku 서비스 자동 배포 (완료)</h1>
-      <h1>MySQL 연동해서 간단한 CRUD 게시판 구현하기 (작업 중)</h1>
+    <div className="app">
+      <h1>아이리스 마켓</h1>
+      <input type="button" value="상품 등록"/>
+      <input type="button" value="등록 제거"/>
     </div>
   );
 }
